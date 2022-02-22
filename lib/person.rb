90df58bb3371c4a94c80b35f660d8b273def0d3b
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-02-21 19:26:03 -0500

    commit tree de40ac8c8483ac5fdab2811b1efae0df1b314269

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+require "pry"
+
+class Person
+    # attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+# beyonce = Person.new
+# beyonce.name = "Beyonce"
+# beyonce.name
